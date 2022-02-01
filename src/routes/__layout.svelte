c925991e40b67104ad44d7cff37005666d426c68
brett <brettbedarf.dev@gmail.com>
2022-01-31 18:25:44 -0700

    configs and base layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Header from '$lib/header.svelte';
+	import '../tailwind.css';
+
+	const twHex = '100';
+</script>
+
+<Header />
+<main class="bg-blue-{twHex}">
+	<slot />
+</main>
+
+<div>
+	<footer>&copy 2022</footer>
+</div>
+
+<style lang="scss">
+	div {
+		footer {
+			background: #333;
+			color: #fff;
+			/* padding: 1em; */
+			text-align: center;
+		}
+	}
+</style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<h1>About Us</h1>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quos vero provident culpa
+	quisquam nobis pariatur hic perferendis voluptate commodi? Id consectetur ipsam maiores
+	perspiciatis expedita voluptatum est facere asperiores.
+</p>
