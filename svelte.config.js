6a6632e3317d5ac18f7b75368be32efbd4728c8c
brett <brettbedarf.dev@gmail.com>
2022-03-06 16:25:00 -0700

    upgrade to latest svelkit version
    wanted to use generated RequestHandler types but does not seem to work
    with .ts files as intended vs jsdoc comments

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
