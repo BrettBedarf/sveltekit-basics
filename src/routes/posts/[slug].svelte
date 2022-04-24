62d0f0ccee3f691ba594d57d353aab8fa028b579
brett <brettbedarf.dev@gmail.com>
2022-04-23 18:28:20 -0600

    13 - load fn

---
+++ src/routes/posts/[slug].svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	// primarilty used to load data before render
+	export const load: Load<{ slug: string }> = async ({ params }) => {
+		const post = {
+			title: params.slug,
+			date: new Date(),
+			body: 'This is my first post!'
+		};
+
+		return {
+			props: {
+				post
+			}
+		};
+	};
+</script>
+
+<script>
+	// post is available because defined in loading function
+	export let post;
+</script>
+
+<h3>{post.title}</h3>
